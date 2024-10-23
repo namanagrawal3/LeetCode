cb168cc9bf065f8749f80c5e4df0e2949c98d0d7
Naman Agrawal <117016403+namanagrawal3@users.noreply.github.com>
2024-10-23 20:29:23 +0530

    Time: 5 ms (65.66%), Space: 44.7 MB (87.1%) - LeetHub

---
+++ 0072-edit-distance/0072-edit-distance.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int[][] dp = new int[word1.length()+1][word2.length()+1];
+        
+        for (int row = 1; row < dp.length; row++) {             // Base-case
+            dp[row][0] = row;
+        }
+        for (int col = 1; col < dp[0].length; col++) {
+            dp[0][col] = col;
+        }
+
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (word1.charAt(i-1) == word2.charAt(j-1))
+                    dp[i][j] = dp[i-1][j-1];
+                else {
+                    int delete = dp[i-1][j];
+                    int replace = dp[i-1][j-1];
+                    int insert = dp[i][j-1];
+                    
+                    dp[i][j] = Math.min(delete, Math.min(replace, insert)) + 1;
+                }
+            }
+        }
+        return dp[dp.length-1][dp[0].length-1];
+    }
+}
