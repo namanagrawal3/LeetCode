c905c552c8764f4774d6f9462a02cfae59666b6e
Naman Agrawal <117016403+namanagrawal3@users.noreply.github.com>
2024-11-05 10:14:50 +0530

    Time: 3 ms (97.78%), Space: 44.4 MB (92.97%) - LeetHub

---
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/3174-minimum-number-of-changes-to-make-binary-string-beautiful.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minChanges(String s) {
+        int n = s.length();
+        int opr = 0;
+
+        for (int i = 0; i < n; i+=2) {
+            if (s.charAt(i) != s.charAt(i+1))
+                opr++;
+        }
+
+        return opr;
+    }
+}
