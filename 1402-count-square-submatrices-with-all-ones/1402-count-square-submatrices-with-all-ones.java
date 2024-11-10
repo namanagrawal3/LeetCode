b5f33facbb5ea3d21ef4e203d6c4d7e3937332f8
Naman Agrawal <117016403+namanagrawal3@users.noreply.github.com>
2024-11-10 11:06:09 +0530

    Time: 6 ms (88.73%), Space: 55 MB (48.22%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.java
@@ -1,27 +1,32 @@
 class Solution {
     public int countSquares(int[][] matrix) {
-        int m = matrix.length;    
-        int n = matrix[0].length; 
-        
-        int[][] dp = new int[m][n];
-        int ans = 0;
-        
-        for (int i = 0; i < m; i++) {
-            dp[i][0] = matrix[i][0];
-            ans += dp[i][0];
-        }
-        for (int j = 1; j < n; j++) {
-            dp[0][j] = matrix[0][j];
-            ans += dp[0][j];
-        }
-        
-        for(int i = 1; i < m; i++) {
-            for(int j = 1; j < n; j++) {
-                if(matrix[i][j] == 1) 
-                    dp[i][j] = 1 + Math.min(Math.min(dp[i][j-1], dp[i-1][j]), dp[i-1][j-1]);
-                ans += dp[i][j];
+        int m = matrix.length;
+        int n = matrix[0].length;
+
+        int[][] dp = new int[m+1][n+1];   
+    // dp[i][j] stores the no.of sqr. submatrices having cell(i,j) at bottom right corner
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (matrix[i-1][j-1] == 0)
+                    dp[i][j] = 0;
+                else {
+                    int left = dp[i][j-1];
+                    int leftDiag = dp[i-1][j-1];
+                    int top = dp[i-1][j];
+
+                    dp[i][j] = 1 + Math.min(left, Math.min(leftDiag, top));
+                }
             }
-        }        
-        return ans;
+        }
+
+        int count = 0;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                count += dp[i][j];
+            }
+        }
+
+        return count;
     }
 }
