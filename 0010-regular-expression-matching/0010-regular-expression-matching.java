31a72557d1a6a12014baefd6fa94cfe96614e9cc
Naman Agrawal <117016403+namanagrawal3@users.noreply.github.com>
2024-11-27 22:29:56 +0530

    Time: 1 ms (100%), Space: 42.4 MB (21.57%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,19 +1,31 @@
 class Solution {
     public boolean isMatch(String s, String p) {
-        return checkFun(s, p, 0, 0);
+        int[][] dp = new int[s.length()+1][p.length()+1];
+        for (int[] r : dp) {
+            Arrays.fill(r, -1);
+        }
+
+        return checkFun(s, p, 0, 0, dp);
     }
-    public boolean checkFun(String s, String p, int i, int j) {
+    public boolean checkFun(String s, String p, int i, int j, int[][] dp) {
         if (j == p.length())
             return i == s.length();
+
+        if (dp[i][j] != -1)
+            return (dp[i][j] == 0) ? false : true;
 
         boolean firstChar = (i < s.length() && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.')) ? true : false;
 
         if (j < p.length()-1 && p.charAt(j+1) == '*') {
-            boolean take = firstChar && checkFun(s, p, i+1, j);
-            boolean not_take = checkFun(s, p, i, j+2);
+            boolean take = firstChar && checkFun(s, p, i+1, j, dp);
+            boolean not_take = checkFun(s, p, i, j+2, dp);
+
+            dp[i][j] = (take || not_take) ? 1 : 0;
             return take || not_take;
         }
         
-        return firstChar && checkFun(s, p, i+1, j+1);
+        boolean ans = firstChar && checkFun(s, p, i+1, j+1, dp);
+        dp[i][j] = (ans) ? 1 : 0;
+        return ans;
     }
 }
