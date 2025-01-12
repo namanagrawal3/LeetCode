e3d5054cd44fd4036e14861cf1cb7df4a35cc79b
Naman Agrawal <117016403+namanagrawal3@users.noreply.github.com>
2025-01-12 19:18:22 +0530

    Time: 13 ms (85.17%), Space: 48.5 MB (7.17%) - LeetHub

---
+++ 0542-01-matrix/0542-01-matrix.java
@@ -0,0 +1,55 @@
+class Solution {
+    public class Pair {
+        int row;
+        int col;
+        int dist;
+        public Pair(int r, int c, int d) {
+            row = r;
+            col = c;
+            dist = d;
+        }
+    }
+    public int[][] updateMatrix(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+
+        int[][] ans = new int[m][n];
+        boolean[][] visited = new boolean[m][n];
+        Queue<Pair> q = new ArrayDeque<>();
+
+        int[] r = {0, 1, 0, -1};
+        int[] c = {1, 0, -1, 0};              
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 0) {
+                    q.add(new Pair(i, j, 0));
+                    visited[i][j] = true;
+                }
+            }
+        }
+
+        while (!q.isEmpty()) {
+            Pair rv = q.poll();
+            int cr = rv.row;
+            int cc = rv.col;
+            int d = rv.dist;
+
+            if (mat[cr][cc] == 1)
+                ans[cr][cc] = d;
+
+            for (int i = 0; i < 4; i++) {
+                int nr = cr + r[i];
+                int nc = cc + c[i];
+                if (nr < 0 || nr >= m || nc < 0 || nc >= n)
+                    continue;
+                if (!visited[nr][nc]) {
+                    q.add(new Pair(nr, nc, d+1));
+                    visited[nr][nc] = true;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
