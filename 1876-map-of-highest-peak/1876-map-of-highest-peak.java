d5c2e745c9c3d608bd71c226c3dea748868d82e9
Naman Agrawal <117016403+namanagrawal3@users.noreply.github.com>
2025-02-12 18:16:11 +0530

    Time: 44 ms (87.01%), Space: 182.2 MB (5.03%) - LeetHub

--- 1876-map-of-highest-peak/1876-map-of-highest-peak.java
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.java
@@ -1,55 +1,39 @@
 class Solution {
-    public class Pair {
-        int row;
-        int col;
-        int dist;
-        public Pair(int r, int c, int d) {
-            row = r;
-            col = c;
-            dist = d;
+    public int[][] highestPeak(int[][] isWater) {
+        int m = isWater.length;
+        int n = isWater[0].length;
+
+        int[][] dir = {{0,1}, {1,0}, {0,-1}, {-1,0}};
+        Queue<int[]> q = new ArrayDeque<>();
+        int[][] height = new int[m][n];
+        for (int[] r : height) {
+            Arrays.fill(r, -1);
         }
-    }
-    public int[][] highestPeak(int[][] mat) {
-        int m = mat.length;
-        int n = mat[0].length;
-
-        int[][] ans = new int[m][n];
-        boolean[][] visited = new boolean[m][n];
-        Queue<Pair> q = new ArrayDeque<>();
-
-        int[] r = {0, 1, 0, -1};
-        int[] c = {1, 0, -1, 0};              
 
         for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (mat[i][j] == 1) {
-                    q.add(new Pair(i, j, 0));
-                    visited[i][j] = true;
+            for (int j = 0;j < n; j++) {
+                if (isWater[i][j] == 1) {
+                    height[i][j] = 0;
+                    q.add(new int[]{i, j});
                 }
             }
         }
 
         while (!q.isEmpty()) {
-            Pair rv = q.poll();
-            int cr = rv.row;
-            int cc = rv.col;
-            int d = rv.dist;
-
-            if (mat[cr][cc] == 0)
-                ans[cr][cc] = d;
-
+            int[] rv = q.poll();
+            int cr = rv[0];
+            int cc = rv[1];
             for (int i = 0; i < 4; i++) {
-                int nr = cr + r[i];
-                int nc = cc + c[i];
-                if (nr < 0 || nr >= m || nc < 0 || nc >= n)
+                int nr = cr + dir[i][0];
+                int nc = cc + dir[i][1];
+                if (nr < 0 || nr >= m || nc < 0 || nc >= n || height[nr][nc] != -1)
                     continue;
-                if (!visited[nr][nc]) {
-                    q.add(new Pair(nr, nc, d+1));
-                    visited[nr][nc] = true;
-                }
+                
+                height[nr][nc] = height[cr][cc] + 1;
+                q.add(new int[]{nr, nc});
             }
         }
-
-        return ans;
+        
+        return height;
     }
 }
