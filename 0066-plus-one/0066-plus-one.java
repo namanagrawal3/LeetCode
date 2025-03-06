fe3701faa2494e1ed6cf5de51c5d1d4aa5377dba
Naman Agrawal <117016403+namanagrawal3@users.noreply.github.com>
2025-03-06 21:37:24 +0530

    Time: 0 ms (100%), Space: 42.1 MB (17.09%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,11 +1,6 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         int n = digits.length;
-        if (digits[n-1] < 9) {
-            digits[n-1]++;
-            return digits;
-        }
-        
         int i = n-1;
         while (i >= 0 && digits[i] == 9) {
             digits[i] = 0;
