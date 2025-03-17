77fe6af8fd42c6e41403d31b0d312256c8c91540
Naman Agrawal <117016403+namanagrawal3@users.noreply.github.com>
2025-03-17 13:56:18 +0530

    Time: 32 ms (7.26%), Space: 122.4 MB (42.95%) - LeetHub

--- 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.java
+++ 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.java
@@ -1,31 +1,57 @@
 class Solution {
     public int minZeroArray(int[] nums, int[][] queries) {
-        int n = nums.length;
-        
-        if (Arrays.stream(nums).allMatch(x -> x == 0)) return 0;
-        int left = 1, right = queries.length;
-        if (!canMakeZeroArray(right, nums, queries)) return -1;
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            if (canMakeZeroArray(mid, nums, queries)) right = mid;
-            else left = mid + 1;
-        }
-        return left;
-    }
+    // For range-updation queries, we will use 'Difference Array' technique
 
-    private boolean canMakeZeroArray(int k, int[] nums, int[][] queries) {
+        if (allZero(nums))
+            return 0;
+
+        int si = 0;
+        int ei = queries.length-1;
+        while (si <= ei) {
+            int mid = si + (ei-si)/2;
+            if (isPossible(mid, nums, queries))
+                ei = mid - 1;
+            else
+                si = mid + 1;
+        }
+
+        return (si == queries.length) ? -1 : si+1;
+    }
+    public boolean isPossible(int idx, int[] nums, int[][] queries) {
         int n = nums.length;
-        int[] diff = new int[n + 1];
-        for (int i = 0; i < k; i++) {
-            int left = queries[i][0], right = queries[i][1], val = queries[i][2];
-            diff[left] += val;
-            diff[right + 1] -= val;
+        int[] diff = new int[n];              // creating the difference array
+        diff[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            diff[i] = nums[i] - nums[i-1];
         }
-        int currVal = 0;
-        for (int i = 0; i < n; i++) {
-            currVal += diff[i];
-            if (currVal < nums[i]) return false;
+
+        for (int i = 0; i <= idx; i++) {
+            int si = queries[i][0];           // Updating the range of difference array  
+            int ei = queries[i][1];
+            int val = queries[i][2];
+
+            diff[si] = diff[si] - val;
+            if (ei+1 < n)
+                diff[ei+1] = diff[ei+1] + val;
         }
-        return true;
+
+        for (int i = 1; i < n; i++) {
+            diff[i] = diff[i] + diff[i-1];
+        }
+
+        // -ve element will show that it is possible but +ve will show it is not possible
+        for (int val : diff) {
+            if (val > 0)                       
+                return false;
+        }
+
+        return true;    
+    }
+    public boolean allZero(int[] nums) {
+        for (int num : nums) {
+            if (num != 0)
+                return false;
+        }
+        return true;    
     }
 }
