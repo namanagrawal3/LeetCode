e6565a9d03f6c2d6c659d3a722f810c528ffa16c
Naman Agrawal <117016403+namanagrawal3@users.noreply.github.com>
2025-03-23 21:08:37 +0530

    Time: 15 ms (71.73%), Space: 65.3 MB (5.62%) - LeetHub

--- 0648-replace-words/0648-replace-words.java
+++ 0648-replace-words/0648-replace-words.java
@@ -1,26 +1,65 @@
 class Solution {
-    public String replaceWords(List<String> dictionary, String sentence) {
-        String[] words = sentence.split(" ");
-        HashSet<String> set = new HashSet<>();
-        StringBuilder ans = new StringBuilder();
+    class Node {
+        char ch;
+        HashMap<Character, Node> child = new HashMap<>();
+        boolean isTerminal = false;
+    }
+    private Node root;
+    public Solution() {
+        root = new Node();
+        root.ch = '*';
+    }
 
-        for (String root : dictionary) {
-            set.add(root);
+    public void insert(String word) {
+        Node curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            if (curr.child.containsKey(ch)) {
+                curr = curr.child.get(ch);
+            }
+            else {
+                Node nn = new Node();
+                nn.ch = ch;
+                curr.child.put(ch, nn);
+                curr = nn;
+            }
         }
 
-        for (String word : words) {
-            StringBuilder temp = new StringBuilder();
-            for (int i = 0; i < word.length(); i++) {
-                char ch = word.charAt(i);
-                temp.append(ch);
+        curr.isTerminal = true;             // marking as Terminal Node (end of the word)
+    }
 
-                if (set.contains(temp.toString()))
-                    break;
+    public String findRoot(String word) {
+        StringBuilder ans = new StringBuilder();
+        Node curr = root;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+
+            if (curr.child.containsKey(ch)) {
+                ans.append(ch);
+                curr = curr.child.get(ch);
+                if (curr.isTerminal)
+                    return ans.toString();
             }
-            ans.append(temp);
+            else
+                return word;
+        }
+
+        return word;
+    }
+    public String replaceWords(List<String> dictionary, String sentence) {
+        Solution tt = new Solution();
+        for (String root : dictionary) {
+            tt.insert(root);
+        }
+
+        String[] words = sentence.split(" ");
+        StringBuilder ans = new StringBuilder();
+
+        for (String word: words) {
+            ans.append(tt.findRoot(word));
             ans.append(" ");
         }
 
-        return ans.toString().trim();
+        return ans.toString().trim();   
     }
 }
