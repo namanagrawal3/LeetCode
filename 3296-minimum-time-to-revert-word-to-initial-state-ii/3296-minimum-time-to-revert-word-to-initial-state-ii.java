a0567bd10ba216d0b8ecbdc5eb01c2f9ffe80009
Naman Agrawal <117016403+namanagrawal3@users.noreply.github.com>
2025-03-30 00:09:41 +0530

    Time: 29 ms (75%), Space: 57.3 MB (23.21%) - LeetHub

---
+++ 3296-minimum-time-to-revert-word-to-initial-state-ii/3296-minimum-time-to-revert-word-to-initial-state-ii.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int minimumTimeToInitialState(String word, int k) {
+    // Check at every multiple of 'k' whether remaining word is same as the prefix or not
+    // thus, use the 'KMP' for finding the LPS
+
+        int n = word.length();
+        int[] dp = new int[n];
+        int len = 0;
+        dp[0] = 0;
+
+        int i = 1;
+        while (i < n) {
+            if (word.charAt(i) == word.charAt(len)) {
+                len++;
+                dp[i] = len;
+                i++;
+            }
+            else {
+                if (len > 0)
+                    len = dp[len - 1];
+                else {
+                    dp[i] = 0;
+                    i++;
+                }
+            }
+        }
+
+        int time = 1;
+        int idx = k;
+        while (idx < n) {
+            int remLen = n - idx;
+            if (dp[n-1] >= remLen && word.indexOf(word.substring(idx)) == 0)
+                break;
+
+            idx = idx + k;
+            time++;
+        }
+
+        return time;    
+    }
+}
