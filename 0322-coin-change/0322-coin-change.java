b49f1cc05c3a895cfa0e61ad594afe8a11d2dff4
Naman Agrawal <117016403+namanagrawal3@users.noreply.github.com>
2025-05-16 21:30:31 +0530

    Time: 32 ms (21.17%), Space: 45.6 MB (15.53%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,29 +1,26 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
-        Arrays.sort(coins);             // for min coins
-
         int[][] dp = new int[n][amount+1];
-        for (int[] r : dp) {
+        for (int[] r: dp) {
             Arrays.fill(r, -1);
         }
 
-        int ans = countFun(coins, n-1, amount, dp);
-        return (ans == Integer.MAX_VALUE - 1) ? -1 : ans;
+        int ans = countFun(coins, 0, amount, dp);
+        return (ans == 100000000) ? -1: ans;
     }
     public int countFun(int[] coins, int idx, int amount, int[][] dp) {
         if (amount == 0)
             return 0;
-        if (idx == -1)
-            return Integer.MAX_VALUE-1;
-
+        if (idx == coins.length)
+            return 100000000;
         if (dp[idx][amount] != -1)
             return dp[idx][amount];
 
-        int take = Integer.MAX_VALUE, not_take = 0;
+        int take = 100000000;
         if (amount >= coins[idx])
             take = 1 + countFun(coins, idx, amount-coins[idx], dp);
-        not_take = countFun(coins, idx-1, amount, dp);
+        int not_take = countFun(coins, idx+1, amount, dp);
 
         return dp[idx][amount] = Math.min(take, not_take);
     }
