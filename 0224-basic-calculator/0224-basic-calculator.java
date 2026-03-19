f4c661e06ee4a92409f7bce92ec4b7d30da65885
Naman Agrawal <117016403+namanagrawal3@users.noreply.github.com>
2026-03-19 23:18:24 +0530

    Time: 8 ms (95.07%), Space: 46.5 MB (67.59%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int calculate(String s) {
+        int n = s.length();
+        Stack<Integer> st = new Stack<>();
+        int num = 0;
+        int result = 0;
+        int sign = 1;           // 1 -> +ve & -1 -> -ve  (initial +ve sign)
+
+        for (int i = 0; i < n; i++) {
+            char ch = s.charAt(i);
+            if (ch >= 48 && ch <= 57) {
+                num = num * 10 + (ch -'0');
+            }
+            else if (ch == '+') {
+                result += (num * sign);
+                sign = 1;
+                num = 0;
+            }
+            else if (ch == '-') {
+                result += (num * sign);
+                sign = -1;
+                num = 0;
+            }
+            else if (ch == '(') {
+                st.push(result);
+                st.push(sign);
+                num = 0;            // evaluate as the beginning
+                result = 0;
+                sign = 1;
+            }
+            else if (ch == ')') {
+                result += (num * sign);
+                int currSign = st.pop();
+                int prevResult = st.pop();
+                result *= currSign;
+                result += prevResult; 
+                num = 0;
+            }
+        }
+
+        result += (num * sign);     // consider "1 + 1"
+        return result;
+    }
+}
