4d233173a9d531cd40365be02303a304ec20030f
Naman Agrawal <117016403+namanagrawal3@users.noreply.github.com>
2026-03-19 22:46:59 +0530

    Time: 1 ms (77.56%), Space: 42.3 MB (76.1%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isHappy(int n) {
+        HashSet<Integer> set = new HashSet<>();
+        set.add(n);
+
+        while (n != 1) {
+            int sum = 0;
+            while (n > 0) {
+                int rem = n % 10;
+                sum += rem * rem;
+                n /= 10;
+            }
+            n = sum;
+            if (set.contains(n))
+                return false;
+            set.add(n);
+        }
+
+        return n == 1;
+    }
+}
