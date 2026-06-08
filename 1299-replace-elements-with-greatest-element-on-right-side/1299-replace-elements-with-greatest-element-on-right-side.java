63fc3156328f99cb0a44d0de4dc7913b5ca31456
Naman Agrawal <117016403+namanagrawal3@users.noreply.github.com>
2026-06-08 20:56:26 +0530

    Time: 3 ms (70.59%), Space: 49 MB (79.95%) - LeetHub

---
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] replaceElements(int[] arr) {
+    // Simple keep track of max element from the end
+     
+        int n = arr.length;
+        int max = -1;
+
+        for (int i = n-1; i >= 0; i--) {
+            int temp = arr[i];
+            arr[i] = max;
+            max = Math.max(max, temp);
+        }
+
+        return arr;
+    }
+}
